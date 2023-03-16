bdec8b679596c5c15cbf43c2a7623203d781881b
MateusSartorio <mateussartorio39@gmail.com>
2023-03-16 15:45:05 -0300

    products and past orders static html structure added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,69 @@
 <template>
   <div class="home">
-    Home
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+        <div  v-for="(product, index) in inventory.slice(0, 3)" :key="product.id" class="card">
+          <div class="card-title">
+            {{ product.name }}
+          </div>
+          <div class="card-body">
+            <i class="icofont-10x icofont-{{ product.icon }}"></i>
+            <form>
+              <div class="row">
+                <div class="cell">
+                  <label>Type:</label>
+                </div>
+                <div class="cell">
+                  <em>{{ product.type }}</em>
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Price:</label>
+                </div>
+                <div class="cell">
+                  ${{ product.price.USD }}
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Quantity:</label>
+                </div>
+                <div class="cell">
+                  <input type="number" v-model.number="product.quantity">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-light" @click="addToCart(product.name, index)">Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import food from '../food.json'
 
 export default defineComponent({
   name: 'HomeView',
+  data () {
+    return {
+      inventory: food
+    }
+  },
   components: {
 
   }
