83dfd2b42d740a27d80bfbe65bd13fd4523cb93f
MateusSartorio <mateussartorio39@gmail.com>
2023-03-15 23:56:40 -0300

    started moving code from html files to vue templates only vue router set up by now

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <RouterLink to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </RouterLink>
+      <RouterLink to="/products" class="top-bar-link">
+        <span>Products</span>
+      </RouterLink>
+      <RouterLink to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </RouterLink>
+    </nav>
+    <!-- <RouterLink @click="toggleSidebar" class="top-bar-cart-link"> -->
+    <!--   <i class="icofont-cart-alt icofont-1x"></i> -->
+    <!--   <span>Cart ({{ Object.keys(cart).length }})</span> -->
+    <!-- </RouterLink> -->
+  </header>
+
   <router-view/>
 </template>
 
