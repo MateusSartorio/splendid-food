b734446be0c2e8eb570fc4b6e4b52062f7297a73
MateusSartorio <mateussartorio39@gmail.com>
2023-03-18 18:50:39 -0300

    all products show on products page sorted by type
    (✅) make the icons that appear on the product cards as well as the carts dynamic
    (✅) display all products on the (-h-o-m-e-) products page, sorted by the type
    () implement login functionality
    () implement past orders functionality

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,9 +1,31 @@
 <template>
   <main class="wrapper">
-    <h1>Products</h1>
+    <h1>Vegetables</h1>
     <div class="card-container">
       <ProductCard
-        v-for="(product, index) in inventory"
+        v-for="(product, index) in inventory.filter(p => p.type === 'vegetable')"
+        :key="product.id"
+        class="card"
+        :index="index"
+        :add-to-cart="addToCart"
+        :product="product"
+      />
+    </div>
+    <h1>Fruits</h1>
+    <div class="card-container">
+      <ProductCard
+        v-for="(product, index) in inventory.filter(p => p.type === 'fruit')"
+        :key="product.id"
+        class="card"
+        :index="index"
+        :add-to-cart="addToCart"
+        :product="product"
+      />
+    </div>
+    <h1>Meat</h1>
+    <div class="card-container">
+      <ProductCard
+        v-for="(product, index) in inventory.filter(p => p.type === 'meat')"
         :key="product.id"
         class="card"
         :index="index"
